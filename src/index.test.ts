48d9e0e2ec071d78373c8d4a6d1eb149b3435de5
Gerard Paapu <gerard@gerardpaapu.com>
2026-06-05 22:22:04 +1200

    fix: uncommented out test

--- src/index.test.ts
+++ src/index.test.ts
@@ -2,11 +2,11 @@ import { expect, describe, it } from 'vitest';
 import { compile } from './index.js';
 
 describe('some examples', () => {
-  // it('compiles to what we expect', () => {
-  //   expect(compile('💚🤍👎🐺-13')).toMatchInlineSnapshot(
-  //     `"-(color:white OR color:green) type:wolf year<=2013"`,
-  //   );
-  // });
+  it('compiles to what we expect', () => {
+    expect(compile('💚🤍👎🐺-13')).toMatchInlineSnapshot(
+      `"-(color:white OR color:green) type:wolf year<=2013"`,
+    );
+  });
 
   it('compiles to what we expect', () => {
     expect(compile('🐻👎🐻🖼️')).toMatchInlineSnapshot(
